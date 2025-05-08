bbec595cba6574abf65740815ca9d5d583ba3681
tkarombang <tuangkarombang@gmail.com>
2025-05-08 13:56:05 +0800

    fourthy-navbarLogo (merubah path svg)

--- src/pages/NavbarPage.tsx
+++ src/pages/NavbarPage.tsx
@@ -16,7 +16,7 @@ const NavbarPage: React.FC<NavbarPageProps> = ({ tournamentName }) => {
     <div className="container mx-auto flex justify-between items-center h-15 sm:h-20">
       <div className="flex items-center justify-center gap-4">
         <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, ease: "easeInOut" }}>
-          <Image src={logo} alt="FIFA World Cup 2026" width={48} height={48} className="w-6 h-auto ml-2 sm:w-10" />
+          <Image src="/assets/fifa-wc-2026.svg" alt="FIFA World Cup 2026" width={48} height={48} className="w-6 h-auto ml-2 sm:w-10" />
         </motion.div>
         <motion.div initial={{ opacity: 0, y: -25 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, ease: "easeOut" }}>
           <Link href="/" className={`relative font-bold transition-all ${router.pathname === "/" ? "text-white" : "hover:text-white text-stone-300"} text-sm font-bold sm:text-xl md:text-2xl uppercase`}>
